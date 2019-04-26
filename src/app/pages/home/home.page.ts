38282ba3cf1813eeaf2e17182119c3d76b97aaf4
fabianjohansson <fabain_j@hotmail.com>
2019-04-26 13:07:25 +0200

    commit for pull

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
   onClick() {
     console.log('Activity clicked');
   }
+
+
 }
