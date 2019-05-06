562976140a5500ceb4e29a3a9193e57991fb5f8c
Douglas Hammarstam <dogge20@hotmail.com>
2019-05-06 16:56:37 +0200

    Merge branch 'master' of https://github.com/vd84/ionicfrontendpvt
    # Conflicts:
    #	src/app/pages/map/map.page.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 import {NativeGeocoder} from '@ionic-native/native-geocoder/ngx';
-import {Geolocation} from '@ionic-native/geolocation';
 
 @NgModule({
     declarations: [AppComponent],
--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,23 +1,18 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {YouthcenterService} from '../../services/youthcenter.service';
+import {Observable} from 'rxjs';
 
 declare var google: any;
-import {Geolocation, GeolocationOptions, Geoposition, PositionError} from '@ionic-native/geolocation';
-import {NavController} from '@ionic/angular';
+
 
 @Component({
     selector: 'app-map',
-    templateUrl: './map.page.html',
-    styleUrls: ['./map.page.scss'],
+    templateUrl: 'map.page.html',
+    styleUrls: ['map.page.scss'],
 })
 export class MapPage implements OnInit {
-    options: GeolocationOptions;
-    currentPos: Geoposition;
-
-    constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+    ngOnInit(): void {
     }
 
-    ngOnInit() {
-    }
 }
-
