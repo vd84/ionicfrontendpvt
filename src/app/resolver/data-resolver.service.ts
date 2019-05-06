f7f1a45dd2709fa581f572e77d57575ff82f6cbb
CWERS <36950971+CWERS@users.noreply.github.com>
2019-05-06 14:27:15 +0200

    DataService and DataResolver

---
+++ src/app/resolver/data-resolver.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
+import {DataService} from '../services/data.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataResolverService implements Resolve<any> {
+
+    constructor(private dataService: DataService) {
+    }
+
+    resolve(route: ActivatedRouteSnapshot) {
+        let id = route.paramMap.get('id');
+        return this.dataService.getData(id);
+    }
+}
