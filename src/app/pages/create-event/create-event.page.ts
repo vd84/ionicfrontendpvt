d5e00ab129181f912e7c77ae1d7caec6cdd42e0f
Douglas Hammarstam <dogge20@hotmail.com>
2019-05-17 16:25:27 +0200

    Merge branch 'master' of https://github.com/vd84/ionicfrontendpvt
    # Conflicts:
    #	src/app/services/activity-service/activity.service.ts

--- src/app/pages/create-event/create-event.page.ts
+++ src/app/pages/create-event/create-event.page.ts
@@ -45,7 +45,7 @@ export class CreateEventPage implements OnInit {
     }
 
     createSuggestion() {
-        this.activityService.addActivity(this.userService.currentUser.id, this.name, this.description, this.userService.currentUser.id, this.alt_location, 0, this.category, this.userService.currentUser.currentyouthcentre, 41); // skickar med suggestion = true (responsible user ska dessutom sättas till något annat.
+        this.activityService.addActivity(this.userService.currentUser.id, this.name, this.description, this.userService.currentUser.id, this.alt_location, 0, this.category, this.userService.currentUser.currentyouthcentre, this.challenged); // skickar med suggestion = true (responsible user ska dessutom sättas till något annat.
         setTimeout(() => {
             this.activityService.getAllMyActivities().subscribe(data => this.activityService.allMyActivities = data);
         }, 25);
@@ -53,7 +53,7 @@ export class CreateEventPage implements OnInit {
     }
 
     createActivity() {
-        this.activityService.addActivity(this.userService.currentUser.id, this.name, this.description, this.userService.currentUser.id, this.alt_location, 0, this.category, this.userService.currentUser.currentyouthcentre, 21); // skickar med suggestion = false
+        this.activityService.addActivity(this.userService.currentUser.id, this.name, this.description, this.userService.currentUser.id, this.alt_location, 0, this.category, this.userService.currentUser.currentyouthcentre, this.challenged); // skickar med suggestion = false
         setTimeout(() => {
             this.activityService.getAllMyActivities().subscribe(data => this.activityService.allMyActivities = data);
         }, 25);
