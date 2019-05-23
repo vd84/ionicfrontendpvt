61ca3147e4cbffe8d4b6ec64aff5518af06ed605
Noura <nouraelk@hotmail.com>
2019-05-23 22:11:44 +0200

    Merge branch 'master' of https://github.com/vd84/ionicfrontendpvt
    # Conflicts:
    #	src/app/pages/profile/profile.page.ts

--- src/app/pages/event/event.page.ts
+++ src/app/pages/event/event.page.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {AuthService} from '../../services/authentication-service/auth.service';
 import {ActivityService} from '../../services/activity-service/activity.service';
 import {DataService} from '../../services/data.service';
 import {getValue} from '@angular/core/src/render3/styling/class_and_style_bindings';
@@ -14,7 +13,7 @@ export class EventPage implements OnInit {
     activity: any;
     hasSearched = false;
     searchedActivities = [];
-    constructor(private router: Router, private authService: AuthService, private activityService: ActivityService, private dataService: DataService) {
+    constructor(private router: Router, private activityService: ActivityService, private dataService: DataService) {
     }
 
 
