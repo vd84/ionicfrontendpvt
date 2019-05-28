2efda1d822260ffa8448c751d2ba3d941192f725
CWERS <36950971+CWERS@users.noreply.github.com>
2019-05-28 10:37:09 +0200

    Merge branch 'master' of https://github.com/vd84/ionicfrontendpvt
    # Conflicts:
    #	src/app/pages/change-password/change-password.page.html
    #	src/app/pages/location/location.page.html
    #	src/app/services/activity-service/activity.service.ts

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,25 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {UserService} from '../../services/user-service/user.service';
+import {ActivityService} from '../../services/activity-service/activity.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
 })
 export class HomePage {
-    constructor(private router: Router) {}
+
+    constructor(private router: Router, private userService: UserService, private activityService: ActivityService) {
+    }
+
+    ionViewWillEnter() {
+        this.activityService.getAllActivities();
+    }
 
     onClick() {
         console.log('Activity clicked');
     }
+
     openYouthCenter() {
         this.router.navigate(['location']);
     }
